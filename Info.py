5ce6416edd0a7c0546102647f2784eb2a1332b44
TindalyTn <104759010+TindalyTn@users.noreply.github.com>
2025-06-28 15:27:18 -0800

    Add files via upload

---
+++ Info.py
@@ -0,0 +1,22 @@
+#Twilio
+
+account_sid = ''
+auth_token = ''
+twilionumber = ''
+twiliosmsnumber = ''
+
+#Telegram Bot Token
+API_TOKEN = "" 
+
+#Host URL
+callurl = '' 
+twiliosmsurl = ''
+
+
+
+
+
+
+
+
+
